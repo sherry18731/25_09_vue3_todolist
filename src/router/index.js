fa7c90adc7b501e6aa2ce6af000805791ea00d78
Sherry Wu <shw18731@gmail.com>
2025-09-07 20:15:32 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import SignUpPage from '@/views/RegisterView.vue'
-import LoginPage from '@/views/TodoListView.vue'
-import TodoList from '@/views/TodoList.vue'
+import LoginPage from '@/views/LoginView.vue'
+import TodoList from '@/views/TodoListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
